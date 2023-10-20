0f34bc8cbae2d8da8a93ccef5b78e62f31f9fb8e
Vinayak1337 <34209962+Vinayak1337@users.noreply.github.com>
2023-10-20 21:58:22 +0530

    making the project working again & fixing a few things

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,50 +3,56 @@ import './App.css';
 import React, { Component } from 'react';
 import Router from '../Router/Router';
 
-// const baseUrl = 'https://picmash-server.herokuapp.com';
-const baseUrl = 'http://localhost:8080';
+const baseUrl = process.env.REACT_APP_BASE_URL || 'http://localhost:5000';
 
 const initialState = {
-  route: 'signin',
-  baseUrl,
-  user: {},
-  signedIn: false,
-}
+	route: 'signin',
+	baseUrl,
+	user: {},
+	signedIn: false
+};
 
 class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      route: 'null',
-      baseUrl,
-      user: {},
-      signedIn: false,
-    }
-  }
-  
-  changeRoute = (route) => {
-    if (route === 'signin') this.setState(initialState);
-    if (route === 'home') this.setState({signedIn: true});
-    else this.setState({signedIn: false});
-    this.setState({route: route});
-  }
+	constructor() {
+		super();
+		this.state = {
+			route: 'null',
+			baseUrl,
+			user: {},
+			signedIn: false
+		};
+	}
 
-  setUser = (user) => {
-    this.setState({user: user});
-  }
+	changeRoute = route => {
+		if (route === 'signin') this.setState(initialState);
+		if (route === 'home') this.setState({ signedIn: true });
+		else this.setState({ signedIn: false });
+		this.setState({ route: route });
+	};
 
-  componentDidMount = () => {
-    if (this.state.user?.id) return this.changeRoute('home');
-    this.changeRoute('signin');
+	setUser = user => {
+		this.setState({ user: user });
+	};
 
-  }
-  render () {
-    return (
-      <div>
-        <Router user={this.state.user} route={this.state.route} changeRoute={this.changeRoute} baseUrl={this.state.baseUrl} setUser={this.setUser}/>
-      </div>
-      );
-    }
+	componentDidMount = () => {
+		if (this.state.user?.id) return this.changeRoute('home');
+		this.changeRoute('signin');
+		fetch(baseUrl); // Just to wake up the server
+	};
+
+	render() {
+		return (
+			<div>
+				<Router
+					user={this.state.user}
+					route={this.state.route}
+					changeRoute={this.changeRoute}
+					baseUrl={this.state.baseUrl}
+					setUser={this.setUser}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/Header/LogoutIcon.jsx
+++ src/Header/LogoutIcon.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class LogoutIcon extends Component {
     render() {
         return (
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-            <path d="M0 0h24v24H0z" fill="none"/>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#0e9ef8">
+                <path d="M0 0h24v24H0z" fill="#0fa2fd"/>
             <path d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"/>
             </svg>
         )
